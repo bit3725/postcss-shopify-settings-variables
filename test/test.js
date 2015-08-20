c8c0dfa4c7f86838834c2e1904ac2479b538d699
bit3725 <bit3725@gmail.com>
2015-08-20 19:09:23 +0800

    update test with latest syntax

--- test/test.js
+++ test/test.js
@@ -17,27 +17,45 @@ describe('postcss-shopify-settings-variables', function () {
 
     /* Write tests here */
 
-    it('replace single variable in value', function () {
-        test('a{ color: $headline_color; }',
-            'a{ color: {{ settings.headline_color}}; }');
+    it('replace single variable in value', function (done) {
+        test('a{ color: $(headline_color); }',
+            'a{ color: {{ settings.headline_color }}; }', { }, done);
     });
 
-    it('replace multiple variables in multiple values', function () {
-        test('a{ color: $headline_color; ' +
-                'background-color: $healine_bg_color; }',
-             'a{ color: {{ settings.headline_color}}; ' +
-                'background-color: {{ settings.healine_bg_color }}; }');
+    it('replace multiple variables in multiple values', function (done) {
+        test('a{ color: $(headline_color); ' +
+                'background-color: $(healine_bg_color); }',
+             'a{ color: {{ settings.headline_color }}; ' +
+                'background-color: {{ settings.healine_bg_color }}; }',
+                { }, done);
+    });
+
+    it('replace single variable in value with pixel unit', function (done) {
+        test('body{ font-size: $(headline_size)px;',
+             'body{ font-size: {{ settings.headline_size }}px; ', { }, done);
+    });
+
+    it('replace single variable in value with liquid filter', function (done) {
+        test('a{ font-size: $(headline_size | divided_by: 2)px;',
+             'a{ font-size: {{ settings.headline_color | divided_by: 2 }}px; ',
+                { }, done);
+    });
+
+    it('replace single variable in value with liquid string filter', function (done) {
+        test('a{ font-family: $(font_family | replace: \'+\', \' \');',
+             'a{ font-family: {{ settings.font_family | replace: \'+\', \' \' }}; ',
+                { }, done);
     });
 
     describe('ignore $ elsewhere', function() {
-        it('ignore $ in property', function () {
+        it('ignore $ in property', function (done) {
             test('a{ $color: headline_color; }',
-                'a{ $color: headline_color; }');
+                'a{ $color: headline_color; }', { }, done);
         });
 
-        it('ignore $ in middle of  value', function () {
+        it('ignore $ in middle of  value', function (done) {
             test('a{ $color: headline_$color; }',
-                'a{ $color: headline_$color; }');
+                'a{ $color: headline_$color; }', { }, done);
         });
     });
 

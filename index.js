62890222ec07e90acc4bfb644ebf8e30e1a41345
bit3725 <bit3725@gmail.com>
2015-08-28 15:47:48 +0800

    support replace relative background url path with shopify asset url filter

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var postcss = require('postcss');
 
+var bgUrlRegex = /url\(\s?['"]?(.*?)['"]?\s?\)/g;
+var protocolRegex = /(https?:)?\/\/|data:/g;
+
 module.exports = postcss.plugin('postcss-shopify-settings-variables',
   function (opts) {
     opts = opts || {};
@@ -11,12 +14,17 @@ module.exports = postcss.plugin('postcss-shopify-settings-variables',
         // Transform CSS AST here
         css.eachInside(function (node) {
             if ( node.type === 'decl' ) {
-                if ( node.value.toString().indexOf('$(') >= 0 ) {
+                var value = node.value.toString();
+                if ( value.indexOf('$(') >= 0 ) {
                     node.value = node.value
                         .replace(/^([^\$]*)(\$\()([^\)]+)(\))(.*)$/,
                             function(match, $1, $2, $3, $4, $5) {
-                                return $1 + '{{ settings.' + $3 + " }}" + $5;
+                                return $1 + '{{ settings.' + $3 + ' }}' + $5;
                             });
+                }
+                if ( bgUrlRegex.test(value) && !protocolRegex.test(value) ) {
+                    node.value = value.replace(bgUrlRegex,
+                        'url({{ "$1" | asset_url }})');
                 }
             }
         });

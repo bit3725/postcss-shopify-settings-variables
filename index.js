ed914bbbb48b8c2d1fb625117587c2c6b94e8f9c
bit3725 <bit3725@gmail.com>
2015-08-28 16:36:56 +0800

    can replace variable and background url together

--- index.js
+++ index.js
@@ -14,16 +14,15 @@ module.exports = postcss.plugin('postcss-shopify-settings-variables',
         // Transform CSS AST here
         css.eachInside(function (node) {
             if ( node.type === 'decl' ) {
-                var value = node.value.toString();
-                if ( value.indexOf('$(') >= 0 ) {
+                if ( node.value.indexOf('$(') >= 0 ) {
                     node.value = node.value
                         .replace(/^([^\$]*)(\$\()([^\)]+)(\))(.*)$/,
                             function(match, $1, $2, $3, $4, $5) {
                                 return $1 + '{{ settings.' + $3 + ' }}' + $5;
                             });
                 }
-                if ( bgUrlRegex.test(value) && !protocolRegex.test(value) ) {
-                    node.value = value.replace(bgUrlRegex,
+                if ( bgUrlRegex.test(node.value) && !protocolRegex.test(node.value) ) {
+                    node.value = node.value.replace(bgUrlRegex,
                         'url({{ "$1" | asset_url }})');
                 }
             }
